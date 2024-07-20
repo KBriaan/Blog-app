fd6fe7f8c09c3b1103102da689621028fe6b0714
K Briaan <kbriaan1@gmail.com>
2024-07-20 14:52:48 +0300

    Mobile menu tap added and given functionality

--- my-blog/src/Components/Navbar.jsx
+++ my-blog/src/Components/Navbar.jsx
@@ -1,12 +1,23 @@
 import { NavLink } from "react-router-dom";
-import { FaBars, FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa6";
+import {
+  FaBars,
+  FaGithub,
+  FaLinkedinIn,
+  FaTwitter,
+  FaXmark,
+} from "react-icons/fa6";
+import { useState } from "react";
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   //nav items
 
   const navItems = [
-    {  path: "/", link: "Home" },
-    {  path: "/services", link: "Services" },
-    {  path: "/about", link: "About" },
+    { path: "/", link: "Home" },
+    { path: "/services", link: "Services" },
+    { path: "/about", link: "About" },
     {
       path: "/blogs",
       link: "Blogs",
@@ -26,23 +37,63 @@ const Navbar = () => {
         <ul className="md:flex gap-12 text-lg hidden">
           {navItems.map(({ path, link }) => (
             <li key={path} className="text-white">
-              <NavLink to={path}>{link}</NavLink>
+              <NavLink
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "active" : ""
+                }
+                to={path}
+              >
+                {link}
+              </NavLink>
             </li>
           ))}
         </ul>
         {/* menu icons */}
 
         <div className="text-white lg:flex gap-4 items-center hidden">
-          <a  href="/" ><FaTwitter className="hover:text-orange-500"/></a>
-          <a  href="/" ><FaLinkedinIn className="hover:text-orange-500"/></a> 
-          <a  href="/" ><FaGithub className="hover:text-orange-500"/></a> 
-          <button className="bg-orange-500 px-6 py-2 font-medium rounded hover:bg-white hover:text-orange-500 transition-all duration-200">Login</button>
+          <a href="/">
+            <FaTwitter className="hover:text-orange-500" />
+          </a>
+          <a href="/">
+            <FaLinkedinIn className="hover:text-orange-500" />
+          </a>
+          <a href="/">
+            <FaGithub className="hover:text-orange-500" />
+          </a>
+          <button className="bg-orange-500 px-6 py-2 font-medium rounded hover:bg-white hover:text-orange-500 transition-all duration-200">
+            Login
+          </button>
         </div>
-{/* mobile menu icon */}
-<div className="md:hidden">
-   <button className="cursor-pointer"> <FaBars className="text-white hover:text-orange-500 w-5 h-5"/> </button> 
-</div >
+        {/* mobile menu icon */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="cursor-pointer">
+            {isMenuOpen ? (
+              <FaXmark className="text-white hover:text-orange-500 w-5 h-5" />
+            ) : (
+              <FaBars className="text-white hover:text-orange-500 w-5 h-5" />
+            )}
+          </button>
+        </div>
       </nav>
+      {/* menu items only for mobile */}
+
+      <div>
+        <ul
+          className={`md:hidden gap-12 text-lg block space-y-4 px-4 py-6 mt-14 bg-white ${
+            isMenuOpen
+              ? "fixed top-0 left-0 w-full transition-all ease-out duration-150"
+              : "hidden"
+          }  `}
+        >
+          {navItems.map(({ path, link }) => (
+            <li key={path} className="text-black">
+              <NavLink onClick={toggleMenu} to={path}>
+                {link}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
     </header>
   );
 };
